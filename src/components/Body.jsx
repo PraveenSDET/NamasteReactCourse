60f70d1a3b5b43e92e43f8c48d3d0509c2080134
PraveenKumar <praveenct08@gmail.com>
2023-02-26 16:44:57 +0530

    Included useState() hook to filter restaurants based on Search input

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,44 @@
+import { useState } from "react"
 import { restaurantList } from "../config"
 import { RestaurantCard } from "./RestaurantCard"
 
-const BodyComponent = () => (
+const BodyComponent = () => {
+    const [searchInput, setSearchInput] = useState("");
+    const [restaurants, setRestaurants] = useState(restaurantList);
+
+    function filterRestaurantsBasedOnSearch(searchInput, restaurants) {
+        return restaurants.filter((restaurant)=> restaurant.data.name.includes(searchInput))
+    }
+
+    return (
+    <>
+    <div className="search-container">
+        <input type="text" className="search-input" placeholder="Search for restaurant" 
+        value={searchInput} 
+        onChange={ (e) => {
+            setSearchInput(e.target.value)
+        }}
+        />
+        <h1>{searchInput}</h1>
+        <button className="search-button"
+         onClick={
+            () => {
+                setRestaurants(filterRestaurantsBasedOnSearch(searchInput, restaurants))
+            }
+         }
+        >Search</button>
+    </div>
     <div className="restaurant-list">
       {
-        restaurantList.map((restaurant) => {
+        restaurants.map((restaurant) => {
           return (
             <RestaurantCard {...restaurant.data}/>
           )
         })
       }
     </div>
+    </>
 )
+}
 
 export default BodyComponent
